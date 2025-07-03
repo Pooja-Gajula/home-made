694a7f3f01aadf02e89e8680cbad3a1dd379dd42
Pooja-Gajula <228x1a4407@khitguntur.ac.in>
2025-07-03 13:35:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -263,4 +263,4 @@ def internal_error(e):
 # -------------------- Run App --------------------
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
